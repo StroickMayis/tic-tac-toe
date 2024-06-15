206fdc4bc37ff3aedd9ddd1a14b4f80c678d04a4
Stroick <erik_o_mueller@yahoo.com>
2024-06-15 18:06:35 -0500

    feat: add checks win conditions & symbol input

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ function convertInputToArray(input){
 
 function writeToGameBoard() {
     let input = convertInputToArray(promptUser());
-    gameBoard[input[0]][input[1]] = `X`;
-    // isDiagonal(input);
+    gameBoard[input[0]][input[1]] = input[2];
 };
 
 function checkRows(){
+    function checkRow(rowNumber){
+        return gameBoard[rowNumber][0] == `X` 
+            && gameBoard[rowNumber][1] == `X`
+            && gameBoard[rowNumber][2] == `X`;
+    }
+
     if(checkRow(0) || checkRow(1) || checkRow(2)){
         return true;
     } else {
@@ -28,32 +33,26 @@ function checkRows(){
     }
 }
 
-function checkRow(rowNumber){
-    return gameBoard[rowNumber][0] == `X` && gameBoard[rowNumber][1] == `X` && gameBoard[rowNumber][2] == `X`;
+function checkColumns(){
+    function checkColumn(columnNumber) {
+        return gameBoard[0][columnNumber] == `X` 
+            && gameBoard[1][columnNumber] == `X` 
+            && gameBoard[2][columnNumber] == `X`;
+    
+    }
+    if(checkColumn(0) || checkColumn(1) || checkColumn(2)){
+        return true;
+    } else {
+        return false;
+    }
 }
 
-while(!checkRows()) {
+function checkDiagonals(){
+    return gameBoard[0][0] == `X` && gameBoard[1][1] == `X` && gameBoard[2][2] == `X`|| 
+           gameBoard[0][2] == `X` && gameBoard[1][1] == `X` && gameBoard[2][0] == `X`;
+}
+
+while(!checkRows() && !checkColumns() && !checkDiagonals()) {
     writeToGameBoard();
     console.log(gameBoard)
 }
-
-// function isDiagonal(input) {
-//     if(checkInput(input,0,1)) {
-//         return false;
-//     }
-//     if(checkInput(input,1,0)) {
-//         return false;
-//     }
-//     if(checkInput(input,1,2)) {
-//         return false;
-//     }
-//     if(checkInput(input,2,1)) {
-//         return false;
-//     }
-//     return true;
-// }
-
-// function checkInput(input,row,col) {
-//     return input[0] == row && input[1] == col;
-// }
-
