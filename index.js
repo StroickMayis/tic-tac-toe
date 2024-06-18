55b453ff5c36af2509188808b6d76bf404a111d3
StroickMayis <erik_o_mueller@yahoo.com>
2024-06-18 12:36:00 -0500

    feat: add checkWin() to work with X's or O's

--- index.js
+++ index.js
@@ -21,13 +21,22 @@ function writeToGameBoard() {
 
 function checkRows(){
     function checkRow(rowNumber){
-        return gameBoard[rowNumber][0] == `X` 
-            && gameBoard[rowNumber][1] == `X`
-            && gameBoard[rowNumber][2] == `X`;
+        if( gameBoard[rowNumber][0] == `X` 
+         && gameBoard[rowNumber][1] == `X`
+         && gameBoard[rowNumber][2] == `X`) {
+            return `X`;
+        }else if( gameBoard[rowNumber][0] == `O` 
+               && gameBoard[rowNumber][1] == `O`
+               && gameBoard[rowNumber][2] == `O`) {
+                   return `O`;
+        }else {
+            return false;
+        }
     }
-
-    if(checkRow(0) || checkRow(1) || checkRow(2)){
-        return true;
+    if(checkRow(0) == `X` || checkRow(1) == `X` || checkRow(2) == `X`) {
+        return `X`;
+    } else if(checkRow(0) == `O` || checkRow(1) == `O` || checkRow(2) == `O`) {
+        return `O`;
     } else {
         return false;
     }
@@ -35,21 +44,49 @@ function checkRows(){
 
 function checkColumns(){
     function checkColumn(columnNumber) {
-        return gameBoard[0][columnNumber] == `X` 
-            && gameBoard[1][columnNumber] == `X` 
-            && gameBoard[2][columnNumber] == `X`;
-    
+        if( gameBoard[0][columnNumber] == `X` 
+         && gameBoard[1][columnNumber] == `X` 
+         && gameBoard[2][columnNumber] == `X` ) {
+            return `X`;
+        }else if( gameBoard[0][columnNumber] == `O` 
+               && gameBoard[1][columnNumber] == `O` 
+               && gameBoard[2][columnNumber] == `O`) {
+                return `O`;
+        }else {
+            return false;
+        }
     }
-    if(checkColumn(0) || checkColumn(1) || checkColumn(2)){
-        return true;
+    if(checkColumn(0) == `X` || checkColumn(1) == `X` || checkColumn(2) == `X`) {
+        return `X`;
+    } else if(checkColumn(0) == `O` || checkColumn(1) == `O` || checkColumn(2) == `O`) {
+        return `O`;
     } else {
         return false;
     }
 }
 
 function checkDiagonals(){
-    return gameBoard[0][0] == `X` && gameBoard[1][1] == `X` && gameBoard[2][2] == `X`|| 
-           gameBoard[0][2] == `X` && gameBoard[1][1] == `X` && gameBoard[2][0] == `X`;
+    if(gameBoard[0][0] == `X` && gameBoard[1][1] == `X` && gameBoard[2][2] == `X`|| 
+       gameBoard[0][2] == `X` && gameBoard[1][1] == `X` && gameBoard[2][0] == `X`) {
+         return `X`;
+    } else if(gameBoard[0][0] == `O` && gameBoard[1][1] == `O` && gameBoard[2][2] == `O`|| 
+              gameBoard[0][2] == `O` && gameBoard[1][1] == `O` && gameBoard[2][0] == `O`) {
+                return `O`;
+    } else {
+        return false;
+    }
+}
+
+function checkWin() {
+    if(checkRows() != false) {
+        return checkRows();
+    } else if(checkColumns() != false) {
+        return checkColumns();
+    } else if(checkDiagonals() != false) {
+        return checkDiagonals();
+    } else {
+        return false;
+    }
 }
 
 while(!checkRows() && !checkColumns() && !checkDiagonals()) {
